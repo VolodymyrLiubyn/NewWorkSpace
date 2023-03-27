359b0c56bef204d6d75e0b54b8554079eb4ccac0
Volodymyr Liubyn <vovalu88@gmail.com>
2023-03-27 20:56:08 +0300

    my array task

--- Tasks/makePairs.js
+++ Tasks/makePairs.js
@@ -1,6 +1,9 @@
 const data = { a: 1, b: 1 };
+const data2 = { a: 1, b: 2 };
 
 function makePairs (a) {
     return Object.entries(a);
 }
+
 console.log(makePairs(data));
+console.log(makePairs(data2));
