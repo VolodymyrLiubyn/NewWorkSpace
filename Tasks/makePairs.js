e1c13970c9e795b4fa2e4eca41926d7cd00619fa
Volodymyr Liubyn <vovalu88@gmail.com>
2023-03-27 21:08:50 +0300

    Task complete

--- Tasks/makePairs.js
+++ Tasks/makePairs.js
@@ -1,9 +1,10 @@
 const data = { a: 1, b: 1 };
 const data2 = { a: 1, b: 2 };
 
-function makePairs (a) {
+  function makePairs (a) {
     return Object.entries(a);
-}
+  }
+
 
 console.log(makePairs(data));
 console.log(makePairs(data2));
