dc63efca8f6f812d9a6019ec18b11ba0aaa8f8f1
Volodymyr Liubyn <vovalu88@gmail.com>
2023-07-26 17:28:59 +0300

    TaskRadency n.2

--- Tasks/TestFirst.js
+++ Tasks/TestFirst.js
@@ -1,26 +1,42 @@
-let s = "radency";
-let p1 = "rdnc";
-let p2 = "aey";
+let s = "w27y7";
+let p1 = "27";
+let p2 = "w7y";
 
 const stringChecker = function (s, p1, p2) {
     
-    const phrase = s.split('');
-    const firstPartOfPhrase = p1.split('');
-    const secondPartOfPhrase = p2.split('');
+    const phrase = Array.from(s);
+    const firstPartOfPhrase = Array.from(p1);
+    const secondPartOfPhrase = Array.from(p2);
+    const firstPartOfPhrasecopy = Array.from(p1);
+    const secondPartOfPhrasecopy = Array.from(p2);
+    
     if (phrase.length !== (firstPartOfPhrase.length + secondPartOfPhrase.length)) {
         return false
     }
     
     let phrase2 = [];
 
+    let phrase3 = [];
+
     for (let i = 0; i < phrase.length; i++) {
 
-        phrase[i] === firstPartOfPhrase[0] ? phrase2.push(firstPartOfPhrase[0]) : phrase2.push(secondPartOfPhrase[0])
+        const condition = phrase[i] === firstPartOfPhrase[0];
+
+        condition ? phrase2.push(firstPartOfPhrase[0]) : phrase2.push(secondPartOfPhrase[0])
                
-        phrase[i] === firstPartOfPhrase[0] ? firstPartOfPhrase.shift(0) : secondPartOfPhrase.shift(0)
+        condition ? firstPartOfPhrase.shift(0) : secondPartOfPhrase.shift(0)
+
+
+        const condition2 = phrase[i] === secondPartOfPhrasecopy[0];
+
+        condition2 ? phrase3.push(secondPartOfPhrasecopy[0]) : phrase3.push(firstPartOfPhrasecopy[0])
+               
+        condition2 ?  secondPartOfPhrasecopy.shift(0) : firstPartOfPhrasecopy.shift(0)
+
     }
     
-    if (phrase.join ('') === phrase2.join ('') ) {
+    if (phrase.join ('') === phrase2.join ('') || phrase3.join ('') ) {
+        
         return true
     }     
     
