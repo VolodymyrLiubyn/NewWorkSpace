82c34c089b39cdbbc95f5514b654a00fab75f96b
Volodymyr Liubyn <vovalu88@gmail.com>
2023-07-26 19:03:02 +0300

    TestRadency n.3

--- Tasks/TestFirst.js
+++ Tasks/TestFirst.js
@@ -1,23 +1,21 @@
-let s = "w27y7";
-let p1 = "27";
-let p2 = "w7y";
+let s = "radency";
+let p1 = "rdnc";
+let p2 = "aey";
 
 const stringChecker = function (s, p1, p2) {
     
     const phrase = Array.from(s);
     const firstPartOfPhrase = Array.from(p1);
     const secondPartOfPhrase = Array.from(p2);
-    const firstPartOfPhrasecopy = Array.from(p1);
-    const secondPartOfPhrasecopy = Array.from(p2);
+    const firstPartOfPhrasecopy = Array.from(p2);
+    const secondPartOfPhrasecopy = Array.from(p1);
     
     if (phrase.length !== (firstPartOfPhrase.length + secondPartOfPhrase.length)) {
         return false
     }
     
     let phrase2 = [];
-
-    let phrase3 = [];
-
+    
     for (let i = 0; i < phrase.length; i++) {
 
         const condition = phrase[i] === firstPartOfPhrase[0];
@@ -25,21 +23,26 @@ const stringChecker = function (s, p1, p2) {
         condition ? phrase2.push(firstPartOfPhrase[0]) : phrase2.push(secondPartOfPhrase[0])
                
         condition ? firstPartOfPhrase.shift(0) : secondPartOfPhrase.shift(0)
+       
+    }
 
+    let phrase3 = [];
+    
+    for (let i = 0; i < phrase.length; i++) {
 
-        const condition2 = phrase[i] === secondPartOfPhrasecopy[0];
+        const condition = phrase[i] === firstPartOfPhrasecopy[0];
 
-        condition2 ? phrase3.push(secondPartOfPhrasecopy[0]) : phrase3.push(firstPartOfPhrasecopy[0])
+        condition ? phrase3.push(firstPartOfPhrasecopy[0]) : phrase3.push(secondPartOfPhrasecopy[0])
                
-        condition2 ?  secondPartOfPhrasecopy.shift(0) : firstPartOfPhrasecopy.shift(0)
-
+        condition ? firstPartOfPhrasecopy.shift(0) : secondPartOfPhrasecopy.shift(0)
+    
     }
     
-    if (phrase.join ('') === phrase2.join ('') || phrase3.join ('') ) {
+    if ((phrase.join ('') === phrase2.join ('')) || (phrase.join ('') === phrase3.join (''))) {
         
         return true
     }     
-    
+
     return false;
 
 }
